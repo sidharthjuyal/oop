5492395486348e9fa7490690ef76124754b5896d
Sidharth juyal <sid.juyal.34@gmail.com>
2022-03-09 00:55:18 +0530

    OOPs

---
+++ extra/encap_extra.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class car{
+    int modelNumber;
+    string company;
+    public:
+    void setValues(int m, string n)
+    {
+        modelNumber=m;
+        company=n;
+    }
+    void getValues()
+    {
+        cout<<modelNumber<<" "<<company<<endl;
+    }
+};
+
+int main()
+{
+   car c;
+   c.setValues(123,"lamborghini");
+   c.getValues();
+}
+
+/*
+ENCAPSULATION 
+-> the act of combining properties and methods, related to the same object, is known as encapsulation.
+-> so that object can be equipped with sufficient info. set and set of operations.
+*/
---
+++ extra/extra_polymorph.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class student{
+   public:
+   void show(int a)  //function overloading
+   {
+       cout<<"Show with parameter."<<endl;
+   }
+   void show()
+   {
+       cout<<"Show without parameter."<<endl;
+   }
+   student()
+   {
+       cout<<"default constructor."<<endl;
+   }
+    student(int a)  //constructor overloading
+   {
+       cout<<"parameterized constructor."<<endl;
+   }
+};
+
+int main()
+{
+    student s1,s2(4);
+    s1.show();
+    s2.show(4);
+}
+
+/*
+POLYMORPHISM
+-> poly-many, morph-forms
+-> when one thing has many forms it is known as polymorphism.
+-> how?
+i) function overloading
+ii) operator overloading
+iii) virtual function
+*/
